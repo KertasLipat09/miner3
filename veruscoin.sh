d9ba11ec664482cf82f83195a1e96827799a626d
KertasLipat09 <45118137+KertasLipat09@users.noreply.github.com>
2022-11-18 19:41:22 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+apt sudo update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RGB9wM2bM6sHkN8pXYUWD2jVPgXx5YgBTZ.001 -p x --cpu 4
